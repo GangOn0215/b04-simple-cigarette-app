00edf1441c9e2f6fd5e31c98f5920f6c834607c8
small _Coin <rmaqlctnekf@naver.com>
2022-06-03 18:41:41 +0900

    css 반응형 추가, view  - 담배 이름 없을때, 담배 종류 추가해주세요 안내문자, option component dropdown state 만들어서 추가 할 담배 종류 버튼을 눌렀을때 white-list 나올수있게 처리 했습니다.

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Option = ({ typesArr, handleWhiteListTypes, toggleHintImg }) => {
+  const [dropdownCigaretteTypes, setDropdownCigaretteTypes] = useState(false);
+  const toggleDropdown = () =>
+    setDropdownCigaretteTypes(!dropdownCigaretteTypes);
+
   return (
-    <div className='option'>
-      <div className='hint'>
+    <div className="option">
+      <div className="hint">
         <label>
-          <input type='checkbox' value={'hint-img'} onClick={toggleHintImg} />
+          <input type="checkbox" value={"hint-img"} onClick={toggleHintImg} />
           hint-img
         </label>
       </div>
-      <div className='white-list'>
+      <div className="menu-dropdown">
+        <button onClick={toggleDropdown}>추가 할 담배 종류</button>
+      </div>
+      <div className={`white-list ${dropdownCigaretteTypes ? "show" : ""}`}>
         {typesArr.map((item, idx) => {
           return (
-            <label className='white-list-types' key={idx}>
-              <input type='checkbox' name={item.name} onChange={handleWhiteListTypes} />
+            <label
+              className={`white-list-types ${
+                dropdownCigaretteTypes ? "show" : ""
+              }`}
+              key={idx}
+            >
+              <input
+                type="checkbox"
+                name={item.name}
+                onChange={handleWhiteListTypes}
+              />
               {item.name_kr}
             </label>
           );
--- src/components/View.js
+++ src/components/View.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import question_mark from '../assets/img/question_mark.png';
+import React from "react";
+import question_mark from "../assets/img/question_mark.png";
 
 const View = ({ cigarette, isHintImg, randomCigarette }) => {
   return (
-    <div className='view-container'>
-      <div className='view-name'>
-        <div className='img-box'>
+    <div className="view-container">
+      <div className="view-name">
+        <div className="img-box">
           {isHintImg ? (
-            <img src={cigarette.img} alt={''} />
+            <img src={cigarette.img} alt={""} />
           ) : (
             <>
-              <img src={question_mark} alt='' />
+              <img src={question_mark} alt="" />
             </>
           )}
         </div>
-        <span>{cigarette.name}</span>
+        <span>
+          {cigarette.name ? cigarette.name : "담배 종류를 추가 해주세요."}
+        </span>
       </div>
 
       <button onClick={randomCigarette}>돌리기</button>
@@ -23,6 +25,6 @@ const View = ({ cigarette, isHintImg, randomCigarette }) => {
 };
 
 View.defaultProps = {
-  cigarette: { img: '', name: '' },
+  cigarette: { img: "", name: "" },
 };
 export default View;
